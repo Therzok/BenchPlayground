d86fc3b474c751e611ee2c46efae1ad92c0e70ec
Marius Ungureanu <therzok@gmail.com>
2019-11-10 01:32:58 +0200

    Add span benchmark

---
+++ BenchPlayground/Benchmarks/runtime/SpanToString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchPlayground.Benchmarks.runtime
+{
+    [MemoryDiagnoser]
+    public class SpanToString
+    {
+        public IEnumerable<string> Items { get; } = new List<string>
+        {
+            new string ('a', 1),
+            new string ('a', 32),
+            new string ('a', 128),
+            new string ('a', 1024 * 16),
+            new string ('a', 1024 * 128),
+        };
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Items))]
+        public string Substring(string input)
+        {
+            return input.Substring(1);
+        }
+
+        [Benchmark(Baseline = true)]
+        [ArgumentsSource(nameof(Items))]
+        public string SliceToString(string input)
+        {
+            return input.AsSpan(1).ToString();
+        }
+    }
+}
