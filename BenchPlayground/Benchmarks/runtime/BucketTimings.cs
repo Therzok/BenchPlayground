2dbf461503c5aff2f531a7e74ce2eb63593551a3
Marius Ungureanu <therzok@gmail.com>
2020-02-15 03:51:08 +0200

    Add BucketTimings benchmark

---
+++ BenchPlayground/Benchmarks/runtime/BucketTimings.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchPlayground
+{
+	public class BucketTimings
+	{
+        static ImmutableArray<int> GenerateBuckets(int maximumValue)
+        {
+			return ImmutableArray.CreateRange(PowerOfTwoSequence(8, maximumValue));
+
+            static IEnumerable<int> PowerOfTwoSequence(int start, int maximumValue)
+            {
+				for (int i = start; i < maximumValue; i *= 2)
+					yield return i;
+            }
+        }
+
+		ImmutableArray<int> BucketLimits;
+		TimeSpan[] ascending, descending, random;
+        
+        [GlobalSetup]
+        public void TestData()
+		{
+			BucketLimits = GenerateBuckets(MaximumValue);
+
+			var currentValues = Enumerable.Range(0, MaximumValue * 2)
+				.Select(x => new TimeSpan(x * TimeSpan.TicksPerMillisecond));
+
+			ascending = currentValues.ToArray();
+			descending = currentValues.Reverse().ToArray();
+			random = currentValues.ToList().Shuffle().ToArray();
+        }
+
+        [Params(8, 32, 128, 512, 2048)]
+        public int MaximumValue { get; set; }
+
+        [Benchmark(Baseline = true)]
+        public Implementation Add()
+        {
+			var bucket = new Implementation(BucketLimits);
+			foreach (var value in ascending)
+				bucket.Add(value);
+			return bucket;
+        }
+
+		[Benchmark]
+		public Implementation AddDescending()
+		{
+			var bucket = new Implementation(BucketLimits);
+			foreach (var value in descending)
+				bucket.Add(value);
+			return bucket;
+		}
+
+		[Benchmark]
+		public Implementation AddRandom()
+		{
+			var bucket = new Implementation(BucketLimits);
+			foreach (var value in random)
+				bucket.Add(value);
+			return bucket;
+		}
+
+		[Benchmark]
+		public Implementation AddLog()
+		{
+			var bucket = new Implementation(BucketLimits);
+			foreach (var value in ascending)
+				bucket.AddLog(value);
+			return bucket;
+		}
+
+		[Benchmark]
+		public Implementation AddLogDescending()
+		{
+			var bucket = new Implementation(BucketLimits);
+			foreach (var value in descending)
+				bucket.AddLog(value);
+			return bucket;
+		}
+
+		[Benchmark]
+		public Implementation AddLogRandom()
+		{
+			var bucket = new Implementation(BucketLimits);
+			foreach (var value in random)
+				bucket.AddLog(value);
+			return bucket;
+		}
+
+		public class Implementation
+		{
+			readonly int[] buckets;
+			readonly ImmutableArray<int> bucketUpperLimit;
+
+			/// <summary>
+			/// Initializes a new instance of the <see cref="T:MonoDevelop.Core.Instrumentation.BucketTimings"/> class,
+			/// taking into account the upper limit bucket counts given.
+			/// </summary>
+			/// <param name="bucketUpperLimit">A sorted array of values to use when trying to bucket.</param>
+			public Implementation(ImmutableArray<int> bucketUpperLimit)
+			{
+				this.bucketUpperLimit = bucketUpperLimit;
+
+				// One more than bucketUpperLimit because the last bucket is everything else.
+				// This number is the max time a keystroke can take to be placed into this bucket
+				buckets = new int[bucketUpperLimit.Length + 1];
+			}
+
+			int CalculateBucket(TimeSpan duration)
+			{
+				long ms = (long)duration.TotalMilliseconds;
+				for (var bucket = 0; bucket < bucketUpperLimit.Length; bucket++)
+				{
+					if (ms <= bucketUpperLimit[bucket])
+					{
+						return bucket;
+					}
+				}
+
+				return buckets.Length - 1;
+			}
+
+			public void Add(TimeSpan duration)
+			{
+				var bucketNumber = CalculateBucket(duration);
+				buckets[bucketNumber]++;
+			}
+
+			public void AddLog(TimeSpan duration)
+			{
+				var bucketNumber = Math.Log(duration.TotalMilliseconds, 2) - 3;
+				var upperLimit = bucketUpperLimit.Length - 1;
+				var bucketNumberNormalized = (bucketNumber < 0) ? 0 : (bucketNumber > upperLimit) ? upperLimit : bucketNumber;
+				buckets[(int)bucketNumberNormalized]++;
+			}
+
+			public void Compare(Implementation other)
+			{
+				for (int i = 0; i < buckets.Length; ++i)
+				{
+					if (buckets[i] != other.buckets[i])
+						throw new Exception();
+				}
+			}
+		}
+	}
+}
---
+++ BenchPlayground/Benchmarks/runtime/BucketTimings.md
@@ -0,0 +1,53 @@
+﻿``` ini
+
+BenchmarkDoBenchmarkDotNet=v0.12.0, OS=macOS 10.15.3 (19D76) [Darwin 19.3.0]
+Intel Core i7-4980HQ CPU 2.80GHz (Haswell), 1 CPU, 8 logical and 4 physical cores
+  [Host]     : Mono 6.10.0.64 (2019-12/0011444f0b6 Tue), X64 
+  DefaultJob : Mono 6.10.0.64 (2019-12/0011444f0b6 Tue), X64 
+
+```
+
+// * Summary *
+
+BenchmarkDotNet=v0.12.0, OS=macOS 10.15.3 (19D76) [Darwin 19.3.0]
+Intel Core i7-4980HQ CPU 2.80GHz (Haswell), 1 CPU, 8 logical and 4 physical cores
+  [Host]     : Mono 6.10.0.64 (2019-12/0011444f0b6 Tue), X64 
+  DefaultJob : Mono 6.10.0.64 (2019-12/0011444f0b6 Tue), X64 
+
+
+|           Method | MaximumValue |         Mean |       Error |      StdDev | Ratio | RatioSD |
+|----------------- |------------- |-------------:|------------:|------------:|------:|--------:|
+|              Add |            8 |     170.9 ns |     0.66 ns |     0.62 ns |  1.00 |    0.00 |
+|    AddDescending |            8 |     171.1 ns |     0.65 ns |     0.61 ns |  1.00 |    0.00 |
+|        AddRandom |            8 |     171.5 ns |     0.73 ns |     0.69 ns |  1.00 |    0.00 |
+|           AddLog |            8 |           NA |          NA |          NA |     ? |       ? |
+| AddLogDescending |            8 |           NA |          NA |          NA |     ? |       ? |
+|     AddLogRandom |            8 |           NA |          NA |          NA |     ? |       ? |
+|                  |              |              |             |             |       |         |
+|              Add |           32 |     843.8 ns |     2.25 ns |     2.10 ns |  1.00 |    0.00 |
+|    AddDescending |           32 |     837.2 ns |     2.06 ns |     1.93 ns |  0.99 |    0.00 |
+|        AddRandom |           32 |     996.7 ns |     3.11 ns |     2.76 ns |  1.18 |    0.00 |
+|           AddLog |           32 |   3,006.6 ns |     4.35 ns |     4.07 ns |  3.56 |    0.01 |
+| AddLogDescending |           32 |   3,028.1 ns |     7.18 ns |     6.00 ns |  3.59 |    0.01 |
+|     AddLogRandom |           32 |   3,262.8 ns |     4.53 ns |     4.02 ns |  3.87 |    0.01 |
+|                  |              |              |             |             |       |         |
+|              Add |          128 |   3,967.5 ns |    14.45 ns |    12.81 ns |  1.00 |    0.00 |
+|    AddDescending |          128 |   3,954.7 ns |    16.08 ns |    15.04 ns |  1.00 |    0.01 |
+|        AddRandom |          128 |   4,477.7 ns |    10.52 ns |     9.33 ns |  1.13 |    0.00 |
+|           AddLog |          128 |  11,788.5 ns |    23.80 ns |    22.27 ns |  2.97 |    0.01 |
+| AddLogDescending |          128 |  11,827.5 ns |    16.74 ns |    15.66 ns |  2.98 |    0.01 |
+|     AddLogRandom |          128 |  13,141.7 ns |    15.14 ns |    14.16 ns |  3.31 |    0.01 |
+|                  |              |              |             |             |       |         |
+|              Add |          512 |  19,162.1 ns |    47.22 ns |    39.43 ns |  1.00 |    0.00 |
+|    AddDescending |          512 |  19,130.4 ns |    56.99 ns |    53.31 ns |  1.00 |    0.00 |
+|        AddRandom |          512 |  21,579.6 ns |    66.59 ns |    62.29 ns |  1.13 |    0.00 |
+|           AddLog |          512 |  46,938.4 ns |    67.30 ns |    62.95 ns |  2.45 |    0.00 |
+| AddLogDescending |          512 |  47,043.4 ns |    69.52 ns |    65.03 ns |  2.46 |    0.01 |
+|     AddLogRandom |          512 |  52,562.8 ns |    70.30 ns |    62.32 ns |  2.74 |    0.01 |
+|                  |              |              |             |             |       |         |
+|              Add |         2048 |  90,255.7 ns |   354.83 ns |   331.90 ns |  1.00 |    0.00 |
+|    AddDescending |         2048 |  90,274.1 ns |   273.13 ns |   255.48 ns |  1.00 |    0.00 |
+|        AddRandom |         2048 | 101,185.9 ns | 1,391.97 ns | 1,302.05 ns |  1.12 |    0.01 |
+|           AddLog |         2048 | 187,002.9 ns |   183.57 ns |   171.71 ns |  2.07 |    0.01 |
+| AddLogDescending |         2048 | 188,299.1 ns |   335.95 ns |   314.24 ns |  2.09 |    0.01 |
+|     AddLogRandom |         2048 | 210,161.8 ns |   279.10 ns |   247.41 ns |  2.33 |    0.01 |
