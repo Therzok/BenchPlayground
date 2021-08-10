5298a5c688219e0c95eead35b00c6568fbde3c3b
Marius Ungureanu <maungu@microsoft.com>
2021-08-10 19:03:04 +0300

    Thing

---
+++ BenchPlayground/Benchmarks/runtime/StringComparison.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchPlayground
+{
+    [MemoryDiagnoser]
+    public class StringComparisonBench
+    {
+        readonly string sourceString = new string('a', 2000);
+
+        public IEnumerable<string> ComparisonStringSource => new[]
+        {
+            new string('a', 1),
+            new string('a', 200),
+            new string('a', 1000),
+            new string('a', 2000),
+            new string('a', 4000),
+        };
+
+        [ParamsSource(nameof(ComparisonStringSource))]
+        public string ComparisonString { get; set; }
+
+        [ParamsAllValues]
+        public StringComparison Comparison { get; set; }
+
+        [Benchmark(Baseline = true)]
+        public bool Bench()
+        {
+            return sourceString.Equals(ComparisonString, Comparison);
+        }
+    }
+}
---
+++ BenchPlayground/Benchmarks/runtime/StringHashCode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchPlayground
+{
+    [MemoryDiagnoser]
+    public class StringHashCode
+    {
+        public IEnumerable<StringComparerWrapper> ComparersSource => new[]
+        {
+            new StringComparerWrapper(StringComparer.CurrentCulture, nameof(StringComparer.CurrentCulture)),
+            new StringComparerWrapper(StringComparer.CurrentCultureIgnoreCase, nameof(StringComparer.CurrentCultureIgnoreCase)),
+
+            new StringComparerWrapper(StringComparer.InvariantCulture, nameof(StringComparer.InvariantCulture)),
+            new StringComparerWrapper(StringComparer.InvariantCultureIgnoreCase, nameof(StringComparer.InvariantCultureIgnoreCase)),
+
+            new StringComparerWrapper(StringComparer.Ordinal, nameof(StringComparer.Ordinal)),
+            new StringComparerWrapper(StringComparer.OrdinalIgnoreCase, nameof(StringComparer.OrdinalIgnoreCase)),
+        };
+
+        public IEnumerable<string> HashCodeStringSource => new[]
+        {
+            new string('a', 1),
+            new string('a', 500),
+            new string('a', 1000),
+        };
+
+        [ParamsSource(nameof(HashCodeStringSource))]
+        public string HashCodeString { get; set; }
+
+        [ParamsSource(nameof(ComparersSource))]
+        public StringComparerWrapper Comparer { get; set; }
+
+        [Benchmark]
+        public int Bench()
+        {
+            return Comparer.GetHashCode(HashCodeString);
+        }
+
+        public sealed class StringComparerWrapper
+        {
+            readonly string name;
+            readonly StringComparer wrapped;
+
+            public StringComparerWrapper(StringComparer wrapped, string name)
+            {
+                this.wrapped = wrapped;
+                this.name = name;
+            }
+
+            public int GetHashCode(string value) => wrapped.GetHashCode(value);
+
+            public override string ToString() => name;
+        }
+    }
+}
