8b2c3fa31069c16bcad13fc4fcf99e3b5510966d
Marius Ungureanu <maungu@microsoft.com>
2021-08-15 16:59:21 +0300

    Add CacheCode sample

---
+++ BenchPlayground/Benchmarks/CacheCode/CacheCode.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchPlayground
+{
+    [GenericTypeArguments(typeof(int))]
+    [GenericTypeArguments(typeof(TimeSpan))]
+    [GenericTypeArguments(typeof(string))]
+    public class CacheCodeBench<T> where T:IEquatable<T>
+    {
+        public T[] ValueArray
+        {
+            get
+            {
+                if (typeof(T) == typeof(int))
+                    return (T[])(object)new[] { 0, byte.MaxValue, short.MaxValue };
+
+                if (typeof(T) == typeof(TimeSpan))
+                    return (T[])(object)new[] { TimeSpan.Zero, TimeSpan.FromHours(2), TimeSpan.FromDays(2) };
+
+                if (typeof(T) == typeof(string))
+                    return (T[])(object)new[] {
+                        string.Join("", Enumerable.Repeat("asdf", 100)),
+                        string.Join("", Enumerable.Repeat("ASDF", 100)),
+                        string.Join("", Enumerable.Repeat("aSdF", 100)),
+                    };
+
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerable<T> ValuesSource => ValueArray;
+
+        [Benchmark(Baseline = true)]
+        [ArgumentsSource(nameof(ValuesSource))]
+        public Dictionary<T, int> LoopUncache(T value)
+        {
+            var dictionary = new Dictionary<T, int>();
+
+            dictionary.Add(value, 0);
+            dictionary[value] = 1;
+            dictionary.Remove(value);
+            return dictionary;
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ValuesSource))]
+        public Dictionary<CacheCode<T>, int> LoopCache(T nocache)
+        {
+            var dictionary = new Dictionary<CacheCode<T>, int>();
+            var value = nocache.CacheCode();
+
+            dictionary.Add(value, 0);
+            dictionary[value] = 1;
+            dictionary.Remove(value);
+            return dictionary;
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(ValuesSource))]
+        public Dictionary<CacheCode<T>, int> LoopCacheEq(T nocache)
+        {
+            var dictionary = new Dictionary<CacheCode<T>, int>(CacheCodeEqualityComparer<T>.Instance);
+            var value = nocache.CacheCode();
+
+            dictionary.Add(value, 0);
+            dictionary[value] = 1;
+            dictionary.Remove(value);
+            return dictionary;
+        }
+    }
+}
---
+++ CacheCode/CacheCode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace System
+{
+    public readonly struct CacheCode<T> : IEquatable<CacheCode<T>> where T:IEquatable<T>
+    {
+        internal readonly T _value;
+        internal readonly int _hashCode;
+
+        public CacheCode(T value, int hashCode)
+        {
+            _value = value;
+            _hashCode = hashCode;
+        }
+
+        public bool Equals(CacheCode<T> other) => _value.Equals(other);
+
+        public override bool Equals(object obj) => obj is CacheCode<T> other && Equals(other);
+
+        public override int GetHashCode() => _hashCode;
+
+        public override string ToString() => _value.ToString();
+    }
+
+    public sealed class CacheCodeEqualityComparer<T> : IEqualityComparer<CacheCode<T>> where T:IEquatable<T>
+    {
+        public static IEqualityComparer<CacheCode<T>> Instance { get; } = new CacheCodeEqualityComparer<T>();
+
+        public bool Equals(CacheCode<T> x, CacheCode<T> y) => x._value.Equals(y._value);
+
+        public int GetHashCode(CacheCode<T> obj) => obj._hashCode;
+    }
+
+    public static class HashCodeExtensions
+    {
+        public static CacheCode<T> CacheCode<T>(this T obj) where T:IEquatable<T>
+        {
+            return new CacheCode<T>(obj, obj.GetHashCode());
+        }
+    }
+}
